ffc576a740ba4b21f176a71d520565a1d5e470c3
Joe Workman <joe@workmanmail.com>
2026-03-29 18:42:34 -0700

    Update inky to v2.0.0-beta.2

--- Formula/inky.rb
+++ Formula/inky.rb
@@ -1,26 +1,26 @@
 class Inky < Formula
   desc "Transform email templates into email-safe HTML"
   homepage "https://github.com/foundation/inky"
-  version "2.0.0-beta.1"
+  version "2.0.0-beta.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/foundation/inky/releases/download/v#{version}/inky-aarch64-apple-darwin.tar.gz"
-      sha256 "713a2405ebc07f1111b4d62090c594877b52d77df28be7379c85e7667e4f2c37"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.2/inky-aarch64-apple-darwin.tar.gz"
+      sha256 "b200603605e5d20839f4674b3fe93145c2c4e84ace79bff3d433f8d5a867b756"
     else
-      url "https://github.com/foundation/inky/releases/download/v#{version}/inky-x86_64-apple-darwin.tar.gz"
-      sha256 "7fdcd64060b9ab9fb2bdae8bf57d7dd09e6e5fd7933b65d5f5dc6f6525b34ff3"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.2/inky-x86_64-apple-darwin.tar.gz"
+      sha256 "0c731266d7ed32d98a2adcb0b1d56add0028945cb30bfddea03022cd0696dfea"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/foundation/inky/releases/download/v#{version}/inky-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "25f2401a5006faa8399fbc9cfd71d30c5cfa0e61898747ef617339a2ca0ae0a3"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.2/inky-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "6c21b754e841ba3579f90b87b23d4c00f5c4b4373719ea50680d75bb546e3248"
     else
-      url "https://github.com/foundation/inky/releases/download/v#{version}/inky-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "42c201efc19955f98b1985680b6a4160237739b9e40e28dfaaf76ebd51e19b8f"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.2/inky-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3b2ef5e10d61907e1ffdab7427a936cd224480b0bccb24857921ebee20bb593a"
     end
   end
 
