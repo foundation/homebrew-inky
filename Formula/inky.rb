63222077a0d64d3ffe3984ffac7d3ece5368a424
Joe Workman <joe@workmanmail.com>
2026-03-30 19:55:03 -0700

    Update inky to v2.0.0-beta.8

--- Formula/inky.rb
+++ Formula/inky.rb
@@ -1,26 +1,26 @@
 class Inky < Formula
   desc "Transform email templates into email-safe HTML"
   homepage "https://github.com/foundation/inky"
-  version "2.0.0-beta.7"
+  version "2.0.0-beta.8"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.7/inky-aarch64-apple-darwin.tar.gz"
-      sha256 "7d2860bd484275d15f158bfb10ce462ec870b11ebdbbeaf1be1de7ce40222f03"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.8/inky-aarch64-apple-darwin.tar.gz"
+      sha256 "9a566768ef7ea53a277e6b218b73365cd734be32bc03705e4b2161e27df5b85b"
     else
-      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.7/inky-x86_64-apple-darwin.tar.gz"
-      sha256 "43a323f755136e0c5a08a381c3c73e94877cfa3306b01931cef39f9ef2877a26"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.8/inky-x86_64-apple-darwin.tar.gz"
+      sha256 "0948b05cd3240880c081f8eae054686d5257f0eb51b7174d0cf8a3691ac56e92"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.7/inky-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "813d3878d8c63ec2d81e94696209e69a105048bdbb06494c27dd4046e512dd1a"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.8/inky-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "e068d572e8453600600fb12517a5160913c1f2a18a5ec12938cfb134d31b2092"
     else
-      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.7/inky-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "957446a12a460e8fc2c9d5f04c3370d8d60514dcce1213853047fe607f1ac43d"
+      url "https://github.com/foundation/inky/releases/download/v2.0.0-beta.8/inky-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "e440d541b367ce2a7869536e0d6469f0401e3f75cbd54f9c86b143d51e29abd1"
     end
   end
 
